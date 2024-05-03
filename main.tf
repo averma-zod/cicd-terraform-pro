8c24a50eb0f23e0757c66c35f6b24ae6a6c14306
A K <avakashverma98@gmail.com>
2024-05-03 10:30:40 +0000

    Update file main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,15 @@ module "web" {
     sn = module.vpc.pb_sn
     sg = module.vpc.mysg
 }
+
+resource "aws_dynamodb_table" "backend" {
+    name           = "backend"
+    read_capacity  = 20
+    write_capacity = 20
+    hash_key       = "gems"
+
+    attribute {
+        name = "backend"
+        type = "S"
+    }
+}
